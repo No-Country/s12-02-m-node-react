618b5a470e34c112e3f926f410f996b58dc9c036
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-11-27 15:08:09 -0300

    dao y controllers listos (publications)

--- server/src/controllers/publicationsController.js
+++ server/src/controllers/publicationsController.js
@@ -2,6 +2,7 @@ import PublicationsManager from '../dao/managerPublications.js';
 import PublicationsModel from '../models/publicationsModel.js';
 const publicationsmanager = new PublicationsManager();
 
+// This controller is for creating a publication
 async function createPublicationController(req, res) {
 	try {
 		const data = req.body;
@@ -16,4 +17,75 @@ async function createPublicationController(req, res) {
 	}
 }
 
-export { createPublicationController };
+// This controller return all publications, the query is optional for data subsets
+async function getAllPublicationsController(req, res) {
+	try {
+		const query = req.query;
+		const allPublications = await PublicationsManager.getAllPublications(query);
+		return res.status(200).send(allPublications);
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+}
+
+// This controller return one publication that match the query
+async function getOnePublicationController(req, res) {
+	try {
+		const query = req.query;
+		const onePublication = await PublicationsManager.getOnePublication(query);
+		return res.status(200).send(onePublication);
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+}
+
+// This controoler update one publication that match the filter
+async function updatePublicationController(req, res) {
+	try {
+		const { dataUpdate, filter } = req.body;
+		const result = await PublicationsManager.updatePublication(filter, dataUpdate);
+		if (result.matchedCount > 0) {
+			const publicationUpdated = await PublicationsManager.getOnePublication(filter);
+			return res.status(200).send(publicationUpdated);
+		} else {
+			return res.status(404).send('No post found to update with that filter');
+		}
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+}
+
+// This controller delete one publication physically or logically
+async function deletePublicationController(req, res) {
+	try {
+		const { mode, filter } = req.query;
+		if (mode === 'physically') {
+			// delete phisically
+			const result = await PublicationsManager.deletePublication(filter);
+			if (result.deletedCoint > 0) {
+				return res.status(200).send('The post has been deleted.');
+			} else {
+				return res.status(404).send('The post not found.');
+			}
+		} else {
+			// Delete logical
+			const result = await PublicationsManager.updatePublication(filter, { active: false });
+			if (result.matchedCount > 0) {
+				const publicationDeleted = await PublicationsManager.getOnePublication(filter);
+				return res.status(200).send(publicationDeleted);
+			} else {
+				return res.status(404).send('No post found to delete with that filter');
+			}
+		}
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+}
+
+export {
+	createPublicationController,
+	getAllPublicationsController,
+	getOnePublicationController,
+	updatePublicationController,
+	deletePublicationController,
+};
--- server/src/dao/managerPublications.js
+++ server/src/dao/managerPublications.js
@@ -20,8 +20,9 @@ class PublicationsManager {
 		this.deleteDocument = deleteDocument;
 	}
 
+	// Create one publication in the DB
 	async createPublication(data) {
-		const { email, title, description, category, photos, date, comment_ID } = data;
+		const { email, title, description, category, photos, date, comment_ID, active } = data;
 		const publication = PublicationsModel({
 			email,
 			title,
@@ -30,8 +31,54 @@ class PublicationsManager {
 			photos,
 			date,
 			comment_ID,
+			active,
 		});
 		await this.createDocument('publicationsCollection', publication);
+	}
+
+	// Get all publications or those publications that match the query
+	async getAllPublications(query) {
+		try {
+			const publications = await this.getAllDocuments('publicationsCollection', query);
+			if (!publications) throw new Error('Not publications avalaibles');
+			return publications;
+		} catch (error) {
+			throw new Error(`Error trying to get posts: ${error.message}`);
+		}
+	}
+
+	// Get only one publication that match the query
+	async getOnePublication(query) {
+		try {
+			const publication = await this.getOneDocument('publicationsCollection', query);
+			return publication;
+		} catch (error) {
+			throw new Error(`Error trying to get post: ${error.message}`);
+		}
+	}
+
+	// Update one publication that match the filter
+	async updatePublication(filter, dataUpdate) {
+		try {
+			const publicationUpdate = await this.updateDocument(
+				'publicationsCollection',
+				filter,
+				dataUpdate
+			);
+			return publicationUpdate;
+		} catch (error) {
+			throw new Error(`Error trying to update post ${error.message}`);
+		}
+	}
+
+	// Delete one publication that match the filter
+	async deletePublication(filter) {
+		try {
+			const publicationDelete = await this.deleteDocument('publicationsCollection', filter);
+			return publicationDelete;
+		} catch (error) {
+			throw new Error(`Error trying to update post ${error.message}`);
+		}
 	}
 }
 
