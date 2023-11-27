9b6cbbd39c5de958933c483c670ca5a7bfd6f06c
santilagos <sntlgs@gmail.com>
2023-11-27 20:17:40 -0300

    delete log

--- server/src/config/factory.js
+++ server/src/config/factory.js
@@ -15,7 +15,6 @@ async function createDocument(collection, data) {
 
 // Funcion para buscar todo
 async function getAllDocuments(collection, query = {}) {
-	console.log(query);
 	try {
 		if (!db[collection]) {
 			await db.connectToDatabase();
