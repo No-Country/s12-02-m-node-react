7d4baf5b75add34b40e7b2e622a5df2e2b7135e4
Pablo2800 <pablopalazzetti@gmail.com>
2023-11-27 13:26:52 -0300

    no sirve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Footer from "./components/organisms/footer/footer";
 function App() {
-  return <p className="w-full bg-black text-white p-3">Proyecto de Eventos</p>;
+  return (
+    <div>
+      <p className="w-full bg-black text-white p-3">Proyecto de Eventos</p>;
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
