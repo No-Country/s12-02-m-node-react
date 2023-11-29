f7769bc75fc2aeb865fff28ecfacb0ef55ff6891
DamianJavierAguirreCerullo <djac441@gmail.com>
2023-11-29 09:55:51 -0300

    Se termino el area de crear eventos

--- client/src/components/molecules/create event/index.jsx
+++ client/src/components/molecules/create event/index.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
+import CreateEventButton from '../../atoms/createEventButton';
 
 function CreateEvent() {
 
 
     return (
-        <div className="w-full bg-primary-1 p-8">
-            <div className="text-center text-black text-5xl font-semibold font-poppins leading-10 mb-4">Crea tu propio evento</div>
+        <div className="w-full bg-primary-1 p-20">
+            <div className="text-center text-black text-5xl font-semibold font-poppins leading-10 mb-4 p-4">Crea tu propio evento</div>
             <div className="text-center text-black text-base font-normal font-poppins leading-normal mb-4">Organiza tus propios eventos de forma rápida y sencilla. </div>
-            <div className="flex justify-center"> {/* Contenedor para centrar el botón */}
-                <button
-                    className="bg-neutral-200 rounded-3xl p-2.5 inline-flex hover:bg-neutral-300 focus:outline-none"
-                >
-                    <div className="text-black text-base font-medium font-poppins tracking-tight">Crear evento</div>
-                </button>
-            </div>
+            <CreateEventButton/>
         </div>
     );
 }
