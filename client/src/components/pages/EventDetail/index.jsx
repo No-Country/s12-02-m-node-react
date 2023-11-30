8d7ce306ea7c838bff842976047c1ecb3118c42e
Pablo2800 <pablopalazzetti@gmail.com>
2023-11-30 12:42:08 -0300

    accordeon y instalacion de next ui

---
+++ client/src/components/pages/EventDetail/index.jsx
@@ -0,0 +1,32 @@
+import { Accordion, AccordionItem } from "@nextui-org/react";
+
+export default function Detail() {
+  const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  return (
+    <div className="w-full">
+      <Accordion variant="splitted" className="my-10">
+        <AccordionItem
+          key="1"
+          aria-label="Accordion 1"
+          title="Accordion 1"
+          className="mt-10"
+        >
+          {defaultContent}
+        </AccordionItem>
+        <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+          {defaultContent}
+        </AccordionItem>
+        <AccordionItem
+          key="3"
+          aria-label="Accordion 3"
+          title="Accordion 3"
+          className="mb-10"
+        >
+          {defaultContent}
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
