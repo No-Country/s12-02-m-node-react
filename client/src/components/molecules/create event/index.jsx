d88bec8f016fa2db21eb6be0ebbc0d2df2cd039e
DamianJavierAguirreCerullo <djac441@gmail.com>
2023-12-04 09:18:09 -0300

    se completo el area de segun tu ubicacion

--- client/src/components/molecules/create event/index.jsx
+++ client/src/components/molecules/create event/index.jsx
@@ -7,11 +7,9 @@ function CreateEvent() {
         Crea tu propio evento
       </div>
       <div className="text-center text-black text-base font-normal font-poppins leading-normal mb-4">
-        Organiza tus propios eventos de forma rápida y sencilla.{" "}
+        Organiza tus propios eventos de forma rápida y sencilla.  
       </div>
       <div className="flex justify-center">
-        {" "}
-        {/* Contenedor para centrar el botón */}
         <button className="bg-neutral-200 rounded-3xl p-2.5 inline-flex hover:bg-neutral-300 focus:outline-none">
           <div className="text-black text-base font-medium font-poppins tracking-tight">
             Crear evento
