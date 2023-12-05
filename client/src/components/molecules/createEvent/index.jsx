e1d1cd89e5d6fc7d94627411dd141c536dde3b62
Caperactus <102335770+CaperAsDev@users.noreply.github.com>
2023-12-05 09:08:58 -0500

    footer responsive, next: upcoming events responsive

--- client/src/components/molecules/createEvent/index.jsx
+++ client/src/components/molecules/createEvent/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function CreateEvent() {
   return (
-    <div className="w-full bg-primary-1 p-24">
-      <div className="text-center text-black text-5xl font-semibold font-poppins leading-10 pt-5 mb-4">
+    <div className="w-full bg-primary-1 p-5">
+      <h2 className="text-center text-black text-4xl font-semibold pt-5 mb-4">
         Crea tu propio evento
-      </div>
-      <div className="text-center text-black text-base font-normal font-poppins leading-normal mb-4">
+      </h2>
+      <div className="text-center text-black text-base font-normal leading-normal mb-4">
         Organiza tus propios eventos de forma rápida y sencilla.  
       </div>
       <div className="flex justify-center">
         <button className="bg-neutral-200 rounded-3xl p-2.5 inline-flex hover:bg-neutral-300 focus:outline-none">
-          <div className="text-black text-base font-medium font-poppins tracking-tight">
+          <NavLink to='/CreateEvent' className="text-base font-medium bg-secondary-2 text-primary-1 p-2 rounded-full hover:scale-110 focus:scale-110 transform transition-transform duration-200 ease-out-expo shadow-md shadow-secondary-3">
             Crear evento
-          </div>
+          </NavLink>
         </button>
       </div>
     </div>
--- client/src/components/organisms/footer/index.jsx
+++ client/src/components/organisms/footer/index.jsx
@@ -1,50 +1,130 @@
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { NavLink } from "react-router-dom";
+
 import { Facebook, Instagram, Youtube } from "lucide-react";
 import { FaTiktok } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { GrLanguage } from "react-icons/gr";
 import { ImHeadphones } from "react-icons/im";
+
 function Footer() {
+  const itemClasses = {
+    base: "py-0 w-full",
+    title: "text-white hover:text-secondary-3",
+    trigger: "px-2 py-0 rounded-lg h-14 flex items-center",
+    indicator: "text-medium",
+    content: "text-small px-2",
+  };
+  const footerLinks = [
+    {
+      linksTitle: "Tu Cuenta",
+      links: [
+        {
+          to: "/",
+          text: "Registrarse",
+        },
+        {
+          to: "/",
+          text: "Iniciar Sesión",
+        },
+        {
+          to: "/",
+          text: "Organizar Evento",
+        },
+        {
+          to: "/",
+          text: "Afiliate",
+        },
+        {
+          to: "/",
+          text: "Ayuda",
+        },
+      ],
+    },
+    {
+      linksTitle: "Descubrir",
+      links: [
+        {
+          to: "/",
+          text: "Eventos en linea",
+        },
+        {
+          to: "/",
+          text: "Cursos y talleres",
+        },
+        {
+          to: "/",
+          text: "Recitales y conciertos",
+        },
+        {
+          to: "/",
+          text: "Guias locales",
+        },
+        {
+          to: "/",
+          text: "Puntos de venta",
+        },
+      ],
+    },
+    {
+      linksTitle: "Sobre Eventwave",
+      links: [
+        {
+          to: "/",
+          text: "Contacto y soporte",
+        },
+        {
+          to: "/",
+          text: "Quienes somos",
+        },
+        {
+          to: "/",
+          text: "Trabaja con nosotros",
+        },
+        {
+          to: "/",
+          text: "Términos y condiciones",
+        },
+        {
+          to: "/",
+          text: "Politica de privacidad",
+        },
+      ],
+    },
+  ];
   return (
     <div className="bg-black w-full flex flex-col text-white items-center ">
-      <div className="flex justify-between w-4/5 py-10">
+      <div className="flex flex-col justify-between w-4/5 py-10">
         <div className="flex justify-center m-2 ">
           <ImHeadphones className="text-2xl mr-1 mt-1" />
           <p className="text-2xl">Event</p>
           <p className="text-gray-400 text-2xl">Wave</p>
         </div>
-        <div className="flex py-5">
-          <div className="flex flex-col items-start justify-center m-2 px-7">
-            <h3 className="mb-2 font-poppins font-bold">Tu cuenta</h3>
-            <button className="my-2 font-poppins">Registrarse</button>
-            <button className="my-2 font-poppins">Iniciar sesión</button>
-            <button className="my-2 font-poppins">Organizar un evento</button>
-            <button className="my-2 font-poppins">Afíliate</button>
-            <button className="my-2 font-poppins">Ayuda</button>
-          </div>
-          <div className="flex flex-col items-start justify-center m-2 px-7">
-            <h3 className="mb-2 font-poppins font-bold">Descubrir</h3>
-            <button className="my-2 font-poppins">Eventos en linea</button>
-            <button className="my-2 font-poppins">Cursos y talleres</button>
-            <button className="my-2 font-poppins">
-              Recitales y conciertos
-            </button>
-            <button className="my-2 font-poppins">Guías locales</button>
-            <button className="my-2 font-poppins">Puntos de venta</button>
-          </div>
-          <div className="flex flex-col items-start justify-center m-2 px-7">
-            <h3 className="mb-2 font-poppins font-bold">Sobre Eventwave</h3>
-            <button className="my-2 font-poppins">Contacto y soporte</button>
-            <button className="my-2 font-poppins">Quienes somos</button>
-            <button className="my-2 font-poppins">Trabaja con nosotros</button>
-            <button className="my-2 font-poppins">
-              Términos y condiciones
-            </button>
-            <button className="my-2 font-poppins">
-              Política de privacidad
-            </button>
-          </div>
-          <div className="flex flex-col items-start m-2">
-            <h3 className="mb-2 font-poppins font-bold">Siguenos</h3>
+        <Accordion variant="light" itemClasses={itemClasses}>
+          {footerLinks.map((section) => (
+            <AccordionItem
+              key={section.linksTitle}
+              aria-label={`${section.linksTitle} Accordion`}
+              title={section.linksTitle}
+              className="text-white"
+            >
+              <ul className="flex flex-col gap-2 items-center">
+                {section.links.map((link) => (
+                  <li>
+                    <NavLink
+                      to={link.to}
+                      className="text-primary-1 text-medium font-thin hover:text-secondary-3 focus:text-secondary-3"
+                    >
+                      {link.text}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </AccordionItem>
+          ))}
+        </Accordion>
+        <div className="flex flex-col items-start m-2">
+            <h3 className="mb-2 font-bold">Siguenos</h3>
             <div className="flex mt-2">
               <Instagram className="m-1" />
               <FaXTwitter className="text-2xl m-1" />
@@ -53,19 +133,58 @@ function Footer() {
               <Youtube className="m-1" />
             </div>
           </div>
-        </div>
+        {/* <div className="flex py-5">
+          <div className="flex flex-col items-start justify-center m-2 px-7">
+            <h3 className="mb-2 font-bold">Tu cuenta</h3>
+            <button className="my-2">Registrarse</button>
+            <button className="my-2">Iniciar sesión</button>
+            <button className="my-2">Organizar un evento</button>
+            <button className="my-2">Afíliate</button>
+            <button className="my-2">Ayuda</button>
+          </div>
+          <div className="flex flex-col items-start justify-center m-2 px-7">
+            <h3 className="mb-2 font-bold">Descubrir</h3>
+            <button className="my-2">Eventos en linea</button>
+            <button className="my-2">Cursos y talleres</button>
+            <button className="my-2">
+              Recitales y conciertos
+            </button>
+            <button className="my-2">Guías locales</button>
+            <button className="my-2">Puntos de venta</button>
+          </div>
+          <div className="flex flex-col items-start justify-center m-2 px-7">
+            <h3 className="mb-2 font-bold">Sobre Eventwave</h3>
+            <button className="my-2">Contacto y soporte</button>
+            <button className="my-2">Quienes somos</button>
+            <button className="my-2">Trabaja con nosotros</button>
+            <button className="my-2">
+              Términos y condiciones
+            </button>
+            <button className="my-2">
+              Política de privacidad
+            </button>
+          </div>
+          <div className="flex flex-col items-start m-2">
+            <h3 className="mb-2 font-bold">Siguenos</h3>
+            <div className="flex mt-2">
+              <Instagram className="m-1" />
+              <FaXTwitter className="text-2xl m-1" />
+              <Facebook className="m-1" />
+              <FaTiktok className="text-2xl m-1" />
+              <Youtube className="m-1" />
+            </div>
+          </div>
+        </div> */}
       </div>
       <div className="flex w-4/5 justify-between items-center py-10 border-t-2 border-white">
         <div>
-          <p className="my-1 font-poppins">
-            EventWare© 2023. Todos los derechos reservados
-          </p>
+          <p className="my-1">EventWare© 2023. Todos los derechos reservados</p>
         </div>
         <div className="flex items-center">
           <GrLanguage className="m-2" />
-          <button className="m-2 font-poppins ">Español </button>
-          <button className="m-2 font-poppins ">English </button>
-          <button className="m-2 font-poppins  ">Portugues </button>
+          <button className="m-2 ">Español </button>
+          <button className="m-2 ">English </button>
+          <button className="m-2  ">Portugues </button>
         </div>
       </div>
     </div>
