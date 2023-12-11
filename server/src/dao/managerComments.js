3568cef094613b5972de0c0e41101de90b25bb46
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-12-11 09:20:12 -0300

    Modelo comments y documentacion back

--- server/src/dao/managerComments.js
+++ server/src/dao/managerComments.js
@@ -1,94 +1,77 @@
+/* eslint-disable camelcase */
 import Database from '../config/mongodb.js';
 import {
 	createDocument,
-	getOneDocument,
-	deleteDocument,
 	getAllDocuments,
+	getOneDocument,
 	updateDocument,
+	deleteDocument,
 } from '../config/factory.js';
-import CommentsModel from '../models/commentsModel.js';
-import parseDateToJSON from '../utils/parseDate.js';
-import { ObjectId } from 'mongodb';
 
-class ManagerComments {
+import CommentsModel from '../models/commentsModel.js';
+
+class CommentsManager {
 	constructor() {
 		this.db = new Database();
 		this.createDocument = createDocument;
-		this.getOneDocument = getOneDocument;
 		this.getAllDocuments = getAllDocuments;
-		this.deleteDocument = deleteDocument;
+		this.getOneDocument = getOneDocument;
 		this.updateDocument = updateDocument;
+		this.deleteDocument = deleteDocument;
 	}
 
-	// Metodo reutilizable para encontrar al usuario
-	findEmail = async email => {
-		const getEmail = await getOneDocument('usersCollection', { email });
-		return getEmail;
-	};
-
-	// Obtener todos los comentarios
-	getAllComments = async () => {
-		const comments = await this.getAllDocuments('commentsCollection');
-
-		if (comments.length === 0) return 'NO_COMMENTS';
-
-		const commentsResponse = comments.map(items => {
-			const { date } = items;
-			return { ...items, date: parseDateToJSON(date) };
+	// Create one comment in the DB
+	async createComment(data) {
+		const { text, email, date, event_ID } = data;
+		const comments = CommentsModel({
+			text,
+			email,
+			date,
+			event_ID,
 		});
+		await this.createDocument('commentsCollection', comments);
+	}
 
-		return commentsResponse;
-	};
+	// Get all comments that match the query
+	async getAllComments(query) {
+		try {
+			const comments = await this.getAllDocuments('commentsCollection', query);
+			if (!comments) throw new Error('No hay comentarios disponibles.');
+			return comments;
+		} catch (error) {
+			throw new Error(`Error al intentar conseguir los comentarios: ${error.message}`);
+		}
+	}
 
-	// Crear un nuevo comentario
-	createNewComment = async (email, body) => {
-		if (!email) return 'EMAIL_NOT_SPECIFIED';
-		if (body.text === '') return 'FIELDS_EMPTY';
+	// Get only one comment that match the query
+	async getOneComment(query) {
+		try {
+			const comment = await this.getOneDocument('commentsCollection', query);
+			return comment;
+		} catch (error) {
+			throw new Error(`Error al intentar conseguir el comentario: ${error.message}`);
+		}
+	}
 
-		const getUser = await this.findEmail(email);
-		if (!getUser) return 'USER_NOT_FOUND';
+	// Update one comment that match the filter
+	async updateComment(filter, dataUpdate) {
+		try {
+			const commentUpdate = await this.updateDocument('commentsCollection', filter, dataUpdate);
+			return commentUpdate;
+		} catch (error) {
+			throw new Error(`Error al intentar actualizar el comentario: ${error.message}`);
+		}
+	}
 
-		const newComment = CommentsModel({ text: body.text, email });
-		await this.createDocument('commentsCollection', newComment);
-
-		const { date } = newComment;
-
-		return { ...newComment._doc, date: parseDateToJSON(date) };
-	};
-
-	// Borrar un comentario
-	deleteComment = async (email, _id) => {
-		if (!email) return 'EMAIL_NOT_SPECIFIED';
-
-		const getUser = await this.findEmail(email);
-		if (!getUser) return 'USER_NOT_FOUND';
-
-		const commentToDelete = await this.getOneDocument('commentsCollection', new ObjectId(_id));
-		if (!commentToDelete) return 'COMMENT_NOT_FOUND';
-
-		await this.deleteDocument('commentsCollection', commentToDelete);
-
-		const { date } = commentToDelete;
-		return { ...commentToDelete, date: parseDateToJSON(date) };
-	};
-
-	// Actualizar un comentario
-	updateComment = async (_id, email, body) => {
-		if (!email) return 'EMAIL_NOT_SPECIFIED';
-
-		const getUser = await this.findEmail(email);
-		if (!getUser) return 'USER_NOT_FOUND';
-
-		const commentToUpdate = await this.getOneDocument('commentsCollection', new ObjectId(_id));
-		if (!commentToUpdate) return 'COMMENT_NOT_FOUND';
-
-		await this.updateDocument('commentsCollection', commentToUpdate, { text: body.text });
-
-		const { date } = commentToUpdate;
-		return { ...commentToUpdate, date: parseDateToJSON(date) };
-	};
+	// Delete one comment that match the filter
+	async deleteComment(filter) {
+		try {
+			const commentDelete = await this.deleteDocument('commentsCollection', filter);
+			return commentDelete;
+		} catch (error) {
+			throw new Error(`Error al intentar eliminar el comentario: ${error.message}`);
+		}
+	}
 }
 
-// Patron singleton para instanciar la clase
-const managerComments = new ManagerComments();
-export default managerComments;
+export default CommentsManager;
