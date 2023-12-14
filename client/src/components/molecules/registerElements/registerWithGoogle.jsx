320c8726e44cb1c6974c1a9ce8d9c6dfa5d94c76
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-12-14 01:16:24 -0300

    Registro c/foto

--- client/src/components/molecules/registerElements/registerWithGoogle.jsx
+++ client/src/components/molecules/registerElements/registerWithGoogle.jsx
@@ -1,54 +1,53 @@
-import { FaGoogle, FaFacebook } from 'react-icons/fa';
-import React, { useState, useEffect } from "react";
+import React, {useState} from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const WithGoogleLogin = () => {
+const WithGoogleLogin = ({ onGoogleLogin, setshowAlertMessage, setAlertMessage }) => {
   const [firebaseConfig, setFirebaseConfig] = useState(null);
-  const [showRegister, setShowRegister] = useState(false);
-  const [user, setUser] = useState(null);  // Nuevo estado para almacenar el usuario
   const navigate = useNavigate();
 
   const handleGoogleLogin = async () => {
-    // Lógica para iniciar sesión con Google
+    try {
+      const response = await axios.get("/login");
+      if (!response.data) {
+        throw new Error("Empty response from server");
+      }
+
+      setFirebaseConfig(response.data);
+      const app = initializeApp(response.data);
+      const auth = getAuth(app);
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      const userData = result.user;
+
       try {
-        const response = await axios.get("/login");
-        if (!response.data) {
-          throw new Error("Empty response from server");
-        }
-  
-        setFirebaseConfig(response.data);
-        const app = initializeApp(response.data);
-        const auth = getAuth(app);
-        const provider = new GoogleAuthProvider();
-        const result = await signInWithPopup(auth, provider);
-  
-        const userData = result.user;
-        setUser(userData);  // Actualizar el estado de usuario
-  
-        try {
-          const registered = await axios.get(
-            `/user/${userData.email}`
-          );
-  
-          if (registered.data.status === 0) {
-            localStorage.setItem("user", JSON.stringify(registered.data.data));
-            localStorage.setItem("token", userData.accessToken);
-            navigate("/");
-          }
-        } catch (error) {
-          if (error.response.data.status === 1) {
-            // MATEO el estado userData tiene los datos para que se los pases al form de registro
-            alert("Usted no está registrado en nuestro sitio, tiene que registrarse")
-          }
-          
+        const registered = await axios.get(`/user/${userData.email}`);
+        onGoogleLogin(userData);
+        if (registered.data.status === 0) {
+          // // alert(
+          // //   "El usuario ya está registrado. Diríjase al logín o cambie de email si desea registrarse con otro."
+          // // );
+          // setAlertMessage("El correo electrónico ya fué registrado, utilice otro para registrarse o inicie sesión.");
+          // setshowAlertMessage(true);
+          // setTimeout(() => {
+          // setshowAlertMessage(false);
+          // }, 5000);
         }
       } catch (error) {
-        console.error("Error signing in with Google:", error.message);
+        if (error.response.data.status === 1) {
+          // Llama a la función de devolución de llamada con los datos del usuario
+          console.log("a registrarse mostro")
+          onGoogleLogin(userData);
+        }
       }
-    
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+      // Llama a la función de devolución de llamada con el usuario como null en caso de error
+      onGoogleLogin(null);
+    }
   };
 
   return (
@@ -57,8 +56,7 @@ const WithGoogleLogin = () => {
         className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-9 rounded focus:outline-none focus:shadow-outline flex items-center w-5/5"
         onClick={handleGoogleLogin}
       >
-        <FaGoogle className="mr-4" />
-        Continuar con Google
+        Registrar cuenta Google
       </button>
     </div>
   );
