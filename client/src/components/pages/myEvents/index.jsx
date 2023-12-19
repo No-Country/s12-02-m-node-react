44a4b8a9118a50ad4cd58118817fc94523409630
emybr <emybr82ar@gmail.com>
2023-12-19 11:14:23 -0300

    actualizada lunes 19

--- client/src/components/pages/myEvents/index.jsx
+++ client/src/components/pages/myEvents/index.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import useFetch from "../../../hooks/useFetch";
 
 function MyEvents() {
+  const [events, setEvents] = useState([]);
+  const userData = JSON.parse(localStorage.getItem('user'));
+  const email = userData.email;
+
+  const [eventsRes, eventsStatus, fetchEvents] = useFetch();
+
+  console.log('Respuesta de la solicitud:', eventsRes);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchEvents({ path:`/event?email=${email}`, method: "GET" });
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchData();
+  }, [email]);
+
+  useEffect(() => {
+    if (eventsStatus.success) {
+      const data2 = eventsRes.data?.document || []; // Asegúrate de tener un array incluso si no hay datos
+      setEvents(data2);
+    }
+  }, [eventsRes, eventsStatus]);
+
   return (
     <div>
-      Hola mis eventos
+      <h1>Mis eventos</h1>
+      {eventsStatus.loading ? (
+        <p>Cargando eventos...</p>
+      ) : eventsStatus.error ? (
+        <p>Error al cargar eventos</p>
+      ) : (
+        <ul>
+          {events.map((event) => (
+            <li key={event._id}>
+              {/* Render each event data here */}
+              {event.description} - {event.email} {/* Reemplaza con las propiedades reales del evento */}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default MyEvents
+export default MyEvents;
+
+
+
+
