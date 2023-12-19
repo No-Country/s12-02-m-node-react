942211ecab861ea22a24b3b9e5fc8803235d853e
Caperactus <102335770+CaperAsDev@users.noreply.github.com>
2023-12-19 16:56:23 -0500

    Detail view working and fully connected with backend

--- client/src/components/atoms/map/index.jsx
+++ client/src/components/atoms/map/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
-const Map = () => {
-    let position = [-34.6144420654301,-58.4458763250916]
+const Map = ({coordinates = [-34.6144420654301,-58.4458763250916]}) => {
+    let position = coordinates
     return (
-        <MapContainer className='h-60 w-60 rounded-2xl z-0' center={position} zoom={13} scrollWheelZoom={false}>
+        <MapContainer className='h-60 w-full rounded-2xl z-0' center={position} zoom={8} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- client/src/components/molecules/comments/viewComments.jsx
+++ client/src/components/molecules/comments/viewComments.jsx
@@ -1,9 +1,27 @@
 import View from "../../atoms/commentsItem/view";
+import { useSelector } from "react-redux";
+import LoadingSkeleton from "../../atoms/loadingSkeleton";
 
-const ViewComments = () => {
-    return (
-        <View />
+const ViewComments = ({commentsStatus}) => {
+  const renderData = ({
+    dataTorender,
+    typeOfSkeleton,
+    customSkeletonClass = "",
+    status = eventStatus,
+  }) =>
+    status.success ? (
+      dataTorender()
+    ) : (
+      <LoadingSkeleton className={customSkeletonClass} type={typeOfSkeleton} />
     );
-  }
+  const comments = useSelector((state) => state.eventDetail.comments);
+  return (
+    <div>
+      {comments.map((comment) =>
+        renderData({ dataTorender: () => <View data={comment} /> , typeOfSkeleton: 'block', status: commentsStatus })
+      )}
+    </div>
+  );
+};
 
-  export default ViewComments
+export default ViewComments;
